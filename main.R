c7c9902212c80af8222f3a16dc2142b80b62565c
millerh1 <millerh1@livemail.uthscsa.edu>
2022-05-12 11:32:05 -0500

    Update instructions

--- main.R
+++ main.R
@@ -1,7 +1,10 @@
+# Modify this code to make the tests pass.
 hello <- function(name=NULL) {
     if (is.null(name)) {
-        return("Hello ?????!")
+        # Should return "Hello world!"
+        return("Hello ?????!")  
     } else {
+        # Should return "Hello <name>!"
         return(paste0("Goodbye ", name, "!"))
     }
 }
